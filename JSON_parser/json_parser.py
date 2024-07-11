a9898a5516b0504b7c02737e275e6cf49357e317
akhand08 <adib08@outlook.com>
2024-07-11 21:27:00 +0600

    valid.json file has been parsed successfully from step2 files. function to retrieve valid string has been added in utils

--- JSON_parser/json_parser.py
+++ JSON_parser/json_parser.py
@@ -4,23 +4,50 @@ import utils
 import json
     
     
-def make_pair(inside_json, object):
+def make_pair(inside_json):
     
+    object = {}
     index = 0
     size = len(inside_json)
     
     
     while index < size:
-        key = ""
-        while inside_json[index] != ":":
+        key = inside_json[index]
+        index += 1
+        while inside_json[index] != '"' and index < size:
             key += inside_json[index]
             index += 1
+        key += inside_json[index]
         
+        while inside_json[index] != ":" and index < size:
+            index += 1
+            
         key = utils.is_real_key(key)
-        if key == False and inside_json[index+1] != " ":
+        
+        
+        if key == False:
             return False
+    
+        
+        
+        value = None
+        index += 1
+        while inside_json[index] == " " and index < size:
+            index += 1
+        
+        
+        index , value = utils.retrieve_value(index,inside_json, size)
+        
+     
+        if value != None:
+            object[key[1:-1]] = value
         else:
-            pass
+            return False
+    
+    return object
+        
+            
+        
             
         
         
@@ -29,9 +56,11 @@ def make_pair(inside_json, object):
         
 def JSON_parse(filename):
     content = utils.read_file(filename)
-    object = {}
+    content = content.strip()
+    # print(content)
     if content != "" and content[0] == "{" and content[-1] == "}":
-        return True
+        object = make_pair(content[1:-1].strip())
+        return object
     else:
         return False
 
@@ -41,6 +70,6 @@ def JSON_parse(filename):
 
 if __name__ == "__main__":
     invalid_json_file = "./tests/step1/invalid.json"
-    valid_json_file = "./tests/step1/valid.json"
+    valid_json_file = "./tests/step2/valid.json"
     print(JSON_parse(valid_json_file))
     
--- JSON_parser/utils.py
+++ JSON_parser/utils.py
@@ -6,6 +6,68 @@ def read_file(filename):
 
 def is_real_key(key):
     if key[0] == '"' and key[-1] == '"':
-        return True
+        return key
     
     return False
+
+def is_valid_boolean(index, inside_json, json_size):
+    value = None
+    while index < json_size and inside_json[index] != ",":
+        value += inside_json[index]
+    value = value.strip()
+    
+    if value == "true":
+        return index, True
+    elif value == "false":
+        return index, False
+    else:
+        return index, "Invalid"
+    
+    
+    
+def is_valid_string(index, inside_json, json_size):
+    value = inside_json[index]
+    index += 1
+    
+    while index < json_size and inside_json[index] != '"':
+        value += inside_json[index]
+        index += 1
+    
+    while index < json_size and inside_json[index] != ",":
+        value += inside_json[index]
+        index += 1
+    
+    
+    value = value.strip()
+    
+    if value[0] == '"' and value[-1] == '"':
+        return index, value[1:-1]
+    
+    return index, None
+        
+
+
+
+def retrieve_value(index, inside_json, json_size):
+    value = None
+    
+    #  checking it is boolen or not
+    
+    if inside_json[index] == "t" or inside_json[index] == "f":
+        index, value = is_valid_boolean(index, inside_json, json_size)
+        
+        if value == "Invalid":
+            return index, None
+        else:
+            index, value
+    
+    if inside_json[index] == '"':
+        index, value = is_valid_string(index, inside_json, json_size)
+    
+        
+        
+        if value == None:
+            return index, None
+        else:
+            return index, value
+        
